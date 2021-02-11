dea3e27d37cd542e0ec84d8d3a9896061a9b6668
Maxi Quoß <maxi@quoss.org>
2021-02-11 21:00:19 +0100

    generate emote html in custom_tags.py

--- polls_project/streamthemen/templatetags/custom_tags.py
+++ polls_project/streamthemen/templatetags/custom_tags.py
@@ -12,7 +12,37 @@ def split(value, arg):
 
 @register.filter(name="match_emote")
 def match_emote(value):
-    find_emote = Emote.objects.filter(code=value)
-    if find_emote:
-        return find_emote.get()
-    return None
+    word_list = value.split(" ")
+    final_html = ""
+
+    for word in word_list:
+        find_emote = Emote.objects.filter(code=word)
+        if find_emote:
+            find_emote = find_emote.get()
+            if find_emote.provider == "twitch":
+                html = """
+                <span class="mx-2 is-align-items-center">
+                    <img src="https://static-cdn.jtvnw.net/emoticons/v1/{}/1.0">
+                </span>
+                """.format(find_emote.emote_id)
+                final_html += html
+            elif find_emote.provider == "bttv":
+                html = """
+                <span class="mx-2 is-align-items-center">
+                    <img src="https://cdn.betterttv.net/emote/{}/1x">
+                </span>
+                """.format(find_emote.emote_id)
+                final_html += html
+            elif find_emote.provider == "ffz":
+                html = """
+                <span class="mx-2 is-align-items-center">
+                    <img src="https://cdn.frankerfacez.com/emote/{}/1">
+                </span>
+                """.format(find_emote.emote_id)
+                final_html += html
+        else:
+            final_html += f"{word} "
+    return final_html
+
+
++=
